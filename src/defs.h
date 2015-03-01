23d95f99b6df75aadb5e2a25ce56b8a4540f67af
onpon4 <onpon4@riseup.net>
2015-03-01 04:48:47 -0500

    Fixed a problem.

--- src/defs.h
+++ src/defs.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Compile-time options
 #ifndef VERSION
-#define VERSION 1.3-dev
+#define VERSION "devbuild"
 #endif
 
 #ifndef DATADIR
